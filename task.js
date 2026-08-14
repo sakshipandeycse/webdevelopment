67f3791624839b609e52b34760e7c3aae07a0ffa
sakshipandeycse <sakshipandeycse@gmail.com>
2026-08-14 11:56:45 +0530

    Day3

---
+++ task.js
@@ -0,0 +1,115 @@
+//Create variable name,id,branch,etc
+
+// var Name="Sakshi Pandey";
+// var id="165";
+// var branch="CSE";
+// var department="AIML";
+// console.log(Name);
+// console.log(id);
+// console.log(branch);
+// console.log(department);
+
+
+////Even or odd using if/else
+//    var a=40;
+// if(a%2==0){
+//     console.log("It is a even number");
+// }
+//  else{
+//     console.log("It is a odd number");
+// } 
+
+//Calculate student percentage
+// console.log("\n==== Percentage ====");
+// function totalMarks(Maths,English,Science){
+//     return Maths+English+Science;
+// }
+// function percentage(total){
+//     return (total/300)*100
+// }
+// var total=totalMarks(80,75,95);
+// console.log("Total Mark:", total);
+// console.log("Percentage=", percentage(total));
+
+
+// Simple Calculator
+// console.log("\n==== SIMPLE CALCULATOR ====");
+// let a = 20;
+// let b = 10;
+// let operator = "+";
+// let result;
+// if (operator == "+") {
+//     result = a + b;
+// }
+// else if (operator == "-") {
+//     result = a - b;
+// }
+// else if (operator == "*") {
+//     result = a * b;
+// }
+// else if (operator == "/") {
+//     result = a / b;
+// }
+// else {
+//     console.log("Invalid operator");
+// }
+// console.log("Result =", result);
+
+//Array
+// console.log("\n==== ARRAY ====");
+// let arr=[10,20,30,40,50];
+// let sum=0;
+// for (let i=0;i<5;i++){
+//     console.log(arr[i]);
+//     sum+=arr[i];
+// }
+// console.log(sum);
+
+//2D ARRAY
+// let student = [
+//     ["Sakshi Pandey", "165", "19", "8.87"],
+//     ["Shawsti Singh", "182", "19", "9"],
+//     ["Rimjhim Kheshari", "154", "19", "9.6"],
+// ];
+// console.log(student);
+
+//Object
+// const student={
+//     id:165,
+//     name:"Sakshi",
+//     course:"AIML"
+// };
+// console.log(student);
+// console.log(student.name);
+
+//object of array
+// const users = [
+//     {
+//         id: 1,
+//         name: "ABC"
+//     },
+//     {
+//         id: 2,
+//         name: "XYZ"
+//     },
+//     {
+//     id: 3,
+//     name: "PQR"
+// }
+// ];
+
+// console.log(users);
+
+//Object Method
+// const employee = {
+//     id: 1,
+//     name: "S",
+//     department: "AIML",
+//     display: function() {
+//         console.log("ID =", this.id);
+//         console.log("Name =", this.name);
+//         console.log("Department =", this.department);
+//     }
+// };
+
+// employee.display();
