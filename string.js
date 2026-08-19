6e1a6fb887affb229891255d400fedab248a6025
sakshipandeycse <sakshipandeycse@gmail.com>
2026-08-19 13:48:05 +0530

    Day4

---
+++ string.js
@@ -0,0 +1,41 @@
+// console.log("=== String Object ===");
+// let name="Sakshi";
+// console.log(name);
+// //String length
+// console.log("Length = ", name.length);
+
+// let str="Welcome Javascript";
+// console.log("Upper Case=", str.toUpperCase());
+// let str="Welcome Javascript";
+// console.log("Lower Case=", str.toLowerCase());
+
+//charAt()
+//let str="Welcome Javascript";
+//console.log("CharAt=", str.charAt(0));
+
+//indexOf()
+//console.log("Index of Java =",str.indexOf("Java"));
+
+//Lastindex()
+//let message = "Javascript is easy. javascriptis powerful";
+//console.log("Last index of Java =",message.lastIndexOf("Javascript"));
+
+//slice
+//console.log("Slice=",message.slice(0,7));
+
+//split
+// let data = "HTML,CSS,JavaScript";
+// let result = data.split(",");
+// console.log("Split=",result);
+
+//replace()
+//let text="I love Java";
+//console.log(text.replace("Java","JavaScript"));
+
+//concat
+let first="Sakshi";
+let last="Pandey";
+let fullName=first.concat(" ",last);
+console.log("Full Name=",fullName);
+
+
